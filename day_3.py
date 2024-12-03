c7ed005f96e52f24b3d7c0c7783fd282b0376c4f
d050150 <tibor.tarnai@sap.com>
2024-12-03 13:55:06 +0100

    day 3

--- day_3.py
+++ day_3.py
@@ -6,16 +6,45 @@ def mul(s):
     return int(nums[0]) * int(nums[1])
 
 
-def calculate_value(line):
+def find_next_stop(current_position, positions):
+    while len(positions) > 1 and positions[0] <= current_position:
+        positions.pop(0)
+
+    if positions:
+        return positions[0]
+    return -1
+
+
+def calculate_value(program):
     total_value = 0
-    for num in re.findall(r"mul\([0-9]{1,3},[0-9]{1,3}\)", line):
-        total_value += mul(num)
+    starts = [s.start() for s in re.finditer(r"do\(\)", program)]
+    ends = [s.start() for s in re.finditer(r"don\'t\(\)", program)]
+    enabled = True
+    current_position, next_stop = 0, 0
+    while True:
+        if enabled:
+            if ends[-1] == next_stop:
+                enabled = True
+                next_stop = len(program)
+            else:
+                next_stop = find_next_stop(current_position, ends)
+                enabled = False
+        else:
+            current_position = find_next_stop(current_position, starts)
+            enabled = True
+            continue
+
+        for num in re.findall(r"mul\([0-9]{1,3},[0-9]{1,3}\)", program[current_position:next_stop]):
+            total_value += mul(num)
+
+        current_position = next_stop
+        if current_position == len(program):
+            break
+
     return total_value
 
 
-total = 0
 with open('day_3.txt') as ifile:
-    for line in ifile:
-        total += calculate_value(line)
+    program = ifile.read()
 
-print(total)
+print(calculate_value(program))
