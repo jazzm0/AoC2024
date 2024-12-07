8c15dee032c967d80d9f4f16c42257fdde225502
Tibor Tarnai <tibor.tarnai@sap.com>
2024-12-07 19:49:34 +0100

    day 7 #1

---
+++ day_7.py
@@ -0,0 +1,26 @@
+equations = []
+
+with open('day_7.txt') as ifile:
+    for line in ifile:
+        equation = line.strip().split(":")
+        equations.append((int(equation[0]), [int(x) for x in equation[1].strip().split(" ")]))
+
+
+def solve(test_result, numbers):
+    results = set()
+    results.add(numbers[0])
+    for i in range(1, len(numbers)):
+        new_results = set()
+        for r in results:
+            new_results.add(r * numbers[i])
+            new_results.add(r + numbers[i])
+        results = new_results
+    return test_result in results
+
+
+result = 0
+for equation in equations:
+    if solve(equation[0], equation[1]):
+        result += equation[0]
+
+print(result)
