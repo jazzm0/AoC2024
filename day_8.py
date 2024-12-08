6cb95023647d06dc8c148aff37f437c8acc9267b
Tibor Tarnai <tibor.tarnai@sap.com>
2024-12-08 20:10:20 +0100

    day 8 #1

--- day_8.py
+++ day_8.py
@@ -1,17 +1,22 @@
-from math import sqrt
-
 antenna_positions = {}
 
 row = 0
-with open('day_8_small.txt') as ifile:
+column = 0
+
+with open('day_8.txt') as ifile:
     for line in ifile:
         line = line.strip()
-        for c in range(len(line)):
+        column = len(line)
+        for c in range(column):
             if line[c] != '.':
                 positions = antenna_positions.get(line[c], set())
                 positions.add((row, c))
                 antenna_positions[line[c]] = positions
         row += 1
+
+
+def in_range(x, y):
+    return 0 <= x <= c and 0 <= y <= row
 
 
 def generate_antinodes(antenna_positions):
@@ -22,10 +27,17 @@ def generate_antinodes(antenna_positions):
                 if first != second:
                     delta_y = second[1] - first[1]
                     delta_x = second[0] - first[0]
-                    distance = sqrt(delta_x ** 2 + delta_y ** 2)
-                    b = first[1] / (first[0] * delta_y) / delta_x
-                    print(distance)
+                    x1, y1 = first[0] - delta_x, first[1] - delta_y
+
+                    if in_range(x1, y1) and (x1, y1) not in positions:
+                        antinodes.add((x1, y1))
+
+                    x2, y2 = second[0] + delta_x, second[1] + delta_y
+
+                    if in_range(x2, y2) and (x2, y2) not in positions:
+                        antinodes.add((x2, y2))
+
     return antinodes
 
 
-print(generate_antinodes(antenna_positions))
+print(len(generate_antinodes(antenna_positions)))
