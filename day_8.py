e3e66d4d4d958cacf05ee82c34ef77c57614b262
d050150 <tibor.tarnai@sap.com>
2024-12-09 09:50:01 +0100

    day 8 #2

--- day_8.py
+++ day_8.py
@@ -19,27 +19,31 @@ def in_range(x, y):
     return 0 <= x < column and 0 <= y < row
 
 
-def generate_antinodes(antenna_positions):
-    antinodes = set()
+def generate_anti_nodes(antenna_positions):
+    anti_nodes = set()
     for frequency, positions in antenna_positions.items():
         for first in positions:
             for second in positions:
                 if first != second:
-                    
+                    anti_nodes.add(first)
+                    anti_nodes.add(second)
+
                     delta_y = second[1] - first[1]
                     delta_x = second[0] - first[0]
 
                     x1, y1 = first[0] - delta_x, first[1] - delta_y
 
-                    if in_range(x1, y1) and (x1, y1) not in positions:
-                        antinodes.add((x1, y1))
+                    while in_range(x1, y1):
+                        anti_nodes.add((x1, y1))
+                        x1, y1 = x1 - delta_x, y1 - delta_y
 
                     x2, y2 = second[0] + delta_x, second[1] + delta_y
 
-                    if in_range(x2, y2) and (x2, y2) not in positions:
-                        antinodes.add((x2, y2))
+                    while in_range(x2, y2):
+                        anti_nodes.add((x2, y2))
+                        x2, y2 = x2 + delta_x, y2 + delta_y
 
-    return antinodes
+    return anti_nodes
 
 
-print(len(generate_antinodes(antenna_positions)))
+print(len(generate_anti_nodes(antenna_positions)))
