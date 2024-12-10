07df8bac0f054cd4f2ee6df2382562fdf9d0db49
d050150 <tibor.tarnai@sap.com>
2024-12-10 11:24:37 +0100

    day 10 #2

--- day_10.py
+++ day_10.py
@@ -12,12 +12,14 @@ with open('day_10.txt') as ifile:
         row += 1
 
 
-def get_trailheads(start):
+def get_trailheads(start, distinct_paths):
     next_location_value = 0
     current_locations = {start}
-    while True:
+    paths = [[start]]
+    while current_locations:
         next_locations = set()
         next_location_value += 1
+        reachable_from_location = {}
         for direction in directions:
             for location in current_locations:
                 row, column = location
@@ -25,16 +27,23 @@ def get_trailheads(start):
                 if 0 <= new_row < len(topographical_map) and 0 <= new_column < len(topographical_map[0]) and \
                         topographical_map[new_row][new_column] == next_location_value:
                     next_locations.add((new_row, new_column))
+                    reachable_from_location.setdefault(location, set()).add((new_row, new_column))
+
+        paths = [path + [loc] for path in paths for loc in next_locations if
+                 path[-1] in reachable_from_location and loc in reachable_from_location[path[-1]]]
 
         current_locations = next_locations
-        if len(next_locations) == 0:
-            return 0
-        elif next_location_value == 9:
+
+        if next_location_value == 9:
+            for path in paths:
+                distinct_paths.add(tuple(path))
             return len(next_locations)
 
 
 total_trailheads = 0
+distinct_paths = set()
 for start in starts:
-    total_trailheads += get_trailheads(start)
+    total_trailheads += get_trailheads(start, distinct_paths)
 
 print(total_trailheads)
+print(len(distinct_paths))
