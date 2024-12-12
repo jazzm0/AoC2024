0cb2c70879e3405ab1a215d9bb0aa40ec3c6061d
d050150 <tibor.tarnai@sap.com>
2024-12-12 10:57:42 +0100

    day 12 #1

---
+++ day_12.py
@@ -0,0 +1,62 @@
+garden = []
+directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+with open('day_12.txt') as ifile:
+    for line in ifile:
+        garden.append([x for x in line.strip()])
+
+
+def explore_region(x, y, regions, explored):
+    if (x, y) in explored:
+        return
+    plant_type = garden[x][y]
+    region = set()
+    next = {(x, y)}
+    while next:
+        x, y = next.pop()
+
+        region.add((x, y))
+        explored.add((x, y))
+        for dx, dy in directions:
+            new_x, new_y = x + dx, y + dy
+            if 0 <= new_x < len(garden) and 0 <= new_y < len(garden[0]) and garden[new_x][new_y] == plant_type and (
+                    new_x, new_y) not in explored:
+                region.add((x, y))
+                next.add((new_x, new_y))
+                explored.add((new_x, new_y))
+    if region:
+        regions.append(region)
+
+
+def calculate_perimeter(region):
+    boundary_points = set()
+
+    for x, y in region:
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+            if (nx, ny) not in region:
+                boundary_points.add((x, y))
+                break
+
+    perimeter = 0
+    for x, y in boundary_points:
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+            if (nx, ny) not in region:
+                perimeter += 1
+
+    return perimeter
+
+
+regions = []
+explored = set()
+
+for x in range(len(garden)):
+    for y in range(len(garden[0])):
+        explore_region(x, y, regions, explored)
+
+total_price = 0
+for region in regions:
+    total_price += len(region) * calculate_perimeter(region)
+
+print(total_price)
