bac13fd927470d6876d81624703ad30d7f3c39e5
d050150 <tibor.tarnai@sap.com>
2024-12-13 13:23:11 +0100

    day 13 #2

--- day_13.py
+++ day_13.py
@@ -1,4 +1,6 @@
 prizes = {}
+new_prizes = {}
+offset = 10 ** 13
 
 increment_a, increment_b = None, None
 with open('day_13.txt') as ifile:
@@ -14,22 +16,33 @@ with open('day_13.txt') as ifile:
         elif line.find("Prize:") != -1:
             prize_coords = line.replace("Prize:", "").strip().split(",")
             prize = (int(prize_coords[0].strip()[2:]), int(prize_coords[1].strip()[2:]))
+            new_prize = (int(prize_coords[0].strip()[2:]) + offset, int(prize_coords[1].strip()[2:]) + offset)
             if increment_a and increment_b:
                 prizes[prize] = (increment_a, increment_b)
+                new_prizes[new_prize] = (increment_a, increment_b)
                 increment_a, increment_b = None, None
 
 
 def solve(x1, y1, x2, y2, x3, y3):
-    for a in range(0, 101):
-        for b in range(0, 101):
-            if a * x1 + b * x2 == x3 and a * y1 + b * y2 == y3:
-                return a, b
+    alpha = (x3 * y2 - x2 * y3) // (x1 * y2 - x2 * y1)
+    beta = (y3 - alpha * y1) // y2
+    if alpha and beta and alpha * x1 + beta * x2 == x3 and alpha * y1 + beta * y2 == y3:
+        return alpha, beta
     return None, None
 
 
 total_cost = 0
 
 for prize, buttons in prizes.items():
+    a, b = solve(buttons[0][0], buttons[0][1], buttons[1][0], buttons[1][1], prize[0], prize[1])
+    if a and b:
+        total_cost += 3 * a + b
+
+print(total_cost)
+
+total_cost = 0
+
+for prize, buttons in new_prizes.items():
     a, b = solve(buttons[0][0], buttons[0][1], buttons[1][0], buttons[1][1], prize[0], prize[1])
     if a and b:
         total_cost += 3 * a + b
