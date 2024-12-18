394dda745b70b6f59367ec1b1ee0ea6e3f9909df
d050150 <tibor.tarnai@sap.com>
2024-12-18 09:18:42 +0100

    day 17 #2

--- day_17.py
+++ day_17.py
@@ -1,4 +1,4 @@
-registers = {"A": 0, "B": 0, "C": 0}
+registers = {}
 program = []
 
 with open('day_17.txt') as ifile:
@@ -8,8 +8,8 @@ with open('day_17.txt') as ifile:
             parts = line.split(" ")
             registers[parts[1][0]] = int(parts[2])
         elif line.find("Program") != -1:
-            for i in line.split(" ")[1].split(","):
-                program.append(int(i))
+            for input in line.split(" ")[1].split(","):
+                program.append(int(input))
 
 
 def get_operand(operand, registers):
@@ -25,11 +25,12 @@ def get_operand(operand, registers):
         raise Exception("Invalid operand")
 
 
-def execute(program, registers, output):
+def execute(program, registers, output, compare=False):
     ip = 0
     while ip < len(program):
         instruction = program[ip]
         operand = program[ip + 1]
+
         if instruction == 0:  # adv
             operand = get_operand(operand, registers)
             registers["A"] = registers["A"] // (2 ** operand)
@@ -45,7 +46,9 @@ def execute(program, registers, output):
         elif instruction == 4:  # bxc
             registers["B"] ^= registers["C"]
         elif instruction == 5:  # out
-            output.append(str(get_operand(operand, registers) % 8))
+            output.append(get_operand(operand, registers) % 8)
+            if compare and program[0:len(output)] != output:
+                return False
         elif instruction == 6:  # bdv
             operand = get_operand(operand, registers)
             registers["B"] = registers["A"] // (2 ** operand)
@@ -53,7 +56,35 @@ def execute(program, registers, output):
             operand = get_operand(operand, registers)
             registers["C"] = registers["A"] // (2 ** operand)
         ip += 2
+    return program == output
 
+
+def get_matches(program, output):
+    match_count = 0
+    while match_count < len(output) and program[match_count] == output[match_count]:
+        match_count += 1
+    return match_count
+
+
+matches = {}
 output = []
 execute(program, registers, output)
-print(",".join(output))
+print(*output, sep=",")
+
+for input in range(1, 10 ** 6):
+    registers["A"] = input
+    registers["B"] = registers["C"] = 0
+    output = []
+    result = execute(program, registers, output, True)
+    match_count = get_matches(program, output)
+    if match_count not in matches:
+        matches[match_count] = [input]
+    else:
+        if len(matches[match_count]) == 10:
+            continue
+        matches[match_count].append(input)
+    if result:
+        print(input)
+        break
+
+print(matches)
